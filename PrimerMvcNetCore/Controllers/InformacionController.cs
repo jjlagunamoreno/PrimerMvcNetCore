ebb27354b8b5eff28ec79726d3c6d94af6520fc0
jjlagunamoreno <jjlagunamoreno@gmail.com>
2025-01-27 11:51:04 +0100

    ejercicios net

---
+++ PrimerMvcNetCore/Controllers/InformacionController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimerMvcNetCore.Models;
+
+namespace PrimerMvcNetCore.Controllers
+{
+    public class InformacionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ControladorVista()
+        {
+            //VAMOS A ENVIAR INFORMACION SIMPLE A NUESTRA VISTA
+            ViewData["NOMBRE"] = "Alumno";
+            ViewData["EDAD"] = 24;
+            ViewBag.DiaSemana = "Lunes";
+            return View();
+        }
+
+        public IActionResult ControladorVistaModel()
+        {
+            Persona persona = new Persona();
+            persona.Nombre = "Alumno";
+            persona.Email = "alumno@email.com";
+            persona.Edad = 27;
+            return View(persona);
+        }
+    }
+}
